320b3d5da3fc5d2d9b889d0d80e82124b01fe6c1
Lance Guyatt <lanceguyatt@gmail.com>
2022-04-21 21:48:56 +0100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vue from '@astrojs/vue';
-import preact from '@astrojs/preact';
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
+import preact from '@astrojs/preact'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://futurestatedesign.surge.sh/',
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), vue(), preact(), partytown()]
-});
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }),
+    vue(),
+    preact(),
+    partytown()
+  ]
+})
