1d5c8369b111124e4243758513f68cc974bbb197
Lance Guyatt <lanceguyatt@gmail.com>
2022-06-09 21:23:40 +0100

    updates

--- src/data/site.js
+++ src/data/site.js
@@ -4,8 +4,9 @@ const description =
   'How to step out of the past to create a business fit for the future'
 
 const url = import.meta.env.PUBLIC_BASE_URL ?? '' // No trailing slash!
-const googleTagManager = import.meta.env.PUBLIC_GTM
-const googleSiteVerification = import.meta.env.PUBLIC_GOOGLE_SITE_VERIFICATION
+const googleTagManager = import.meta.env.PUBLIC_GTM ?? ''
+const googleSiteVerification =
+  import.meta.env.PUBLIC_GOOGLE_SITE_VERIFICATION ?? ''
 
 export const image = {
   url: `${url}/images/book-1.jpg`,
