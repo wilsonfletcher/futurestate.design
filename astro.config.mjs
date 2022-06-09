44f959fd4617b6b26fd4aefc18b40b732525220c
Lance Guyatt <lanceguyatt@gmail.com>
2022-06-09 20:37:04 +0100

    partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import vue from '@astrojs/vue'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+import preact from '@astrojs/preact';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://futurestate.design/',
-  integrations: [
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      config: { applyBaseStyles: false }
-    }),
-    vue(),
-    preact()
-  ],
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: {
+      applyBaseStyles: false
+    }
+  }), vue(), preact(), partytown()],
   markdown: {
     drafts: true
   }
-})
+});
