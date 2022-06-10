3be90269312676ec886eb5182c20976eca4f9d26
Lance Guyatt <lanceguyatt@gmail.com>
2022-06-10 14:27:21 +0100

    tidy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vue from '@astrojs/vue';
-import preact from '@astrojs/preact';
-
-import astro from "astro-compress";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
+import preact from '@astrojs/preact'
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://futurestate.design/',
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), vue(), preact(), astro()],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }),
+    vue(),
+    preact(),
+    compress()
+  ],
   markdown: {
     drafts: true
   }
-});
+})
